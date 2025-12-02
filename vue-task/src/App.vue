237f4cfee5ee726cca3a36a6511fcfa3a8458b1b
Student1 <rafiaakterbonna@gmail.com>
2025-12-02 12:33:43 +0600

    first commit

---
+++ vue-task/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+    };
+  }
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>Vue Task Example</h1>
+
+    <input 
+      type="text" 
+      v-model="name"
+      placeholder="Type your name..."
+    />
+
+    <h2>
+      Hello, {{ name ? name : "Guest" }}!
+    </h2>
+  </div>
+</template>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 60px;
+}
+input {
+  padding: 10px;
+  font-size: 18px;
+}
+</style>
