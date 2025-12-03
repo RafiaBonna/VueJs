f14d2db7aef47e1aa74cb12b3e2878ccc8edc4c3
Student1 <rafiaakterbonna@gmail.com>
2025-12-03 12:59:14 +0600

    my

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,52 @@
+<script setup>
+import { ref } from 'vue'
+import logo from './assets/logo.png'
+</script>
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="#">My Vue App</a>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <!-- Logo -->
+      <router-link to="/" class="navbar-brand">
+        <img :src="logo" alt="Logo" height="40" class="d-inline-block align-text-top">
+        
+      </router-link>
 
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link" exact>Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/about" class="nav-link">About</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/contact" class="nav-link">Contact</router-link>
-            </li>
-          </ul>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link" exact>Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact" class="nav-link">Contact</router-link>
+          </li>
+        </ul>
       </div>
-    </nav>
-
-    <div class="container mt-4">
-      <router-view />
     </div>
+  </nav>
+
+  <div class="container mt-4">
+    <router-view />
   </div>
 </template>
 
-<script setup></script>
 
 <style scoped>
-/* Optional: Active link highlight */
+/* Active link highlight */
 .nav-link.router-link-active {
   font-weight: bold;
   text-decoration: underline;
