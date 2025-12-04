6115701931f267838e016ff220edd9c40157aaf9
Student1 <rafiaakterbonna@gmail.com>
2025-12-04 12:48:32 +0600

    my

---
+++ vue-project/src/views/Info.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ text}}</h1>
+        <!-- <h1>"This is number":{{ cars[0] }}</h1> -->
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            count:0,
+            text:"This is home page",
+            numbers:34,
+            cars:[2,4,7,9],
+            object:{
+                name:"BMW",
+                model:"3964"
+            }
+        }
+    }
+}
+</script>
