e3f9a54d3aa8cc4745abb1e5dc6cb43b40dba190
RafiaBonna <rafiaakterbonna@gmail.com>
2025-12-10 23:46:37 +0600

    my

--- vue-task/src/views/Homeview.vue
+++ vue-task/src/views/Homeview.vue
@@ -10,7 +10,7 @@ export default{
     data(){
         return{
             count:0,
-            text:"This is a home page",
+            text:"This is home page",
             numbers:34,
             cars:[2,4,7,9],
             object:{
