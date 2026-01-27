b6dcfca1f1b755b8951815a6a78a25b5e6d9c43a
RafiaBonna <rafiaakterbonna@gmail.com>
2026-01-27 23:53:24 +0600

    my

--- vue-task/src/views/About.vue
+++ vue-task/src/views/About.vue
@@ -12,7 +12,7 @@ export default{
             count:0,
             text:"This is a About Page",
             numbers:34,
-            cars:[1,2,3,4,5,],
+            cars:[1,2,3,4,],
             object:{
                 name:"BMW",
                 model:"3964"
