7d4fb9738193b26361a4ef1abcf08db198baee0c
RafiaBonna <rafiaakterbonna@gmail.com>
2026-02-01 23:35:53 +0600

    my

--- vuejs-laravel-12-crud/vue-frontend/src/CategoryEditForm.vue
+++ vuejs-laravel-12-crud/vue-frontend/src/CategoryEditForm.vue
@@ -74,7 +74,7 @@ export default {
             </div>
 
             <div class="form-group">
-                <label for="description-edit">Description 123456789:</label>
+                <label for="description-edit">Description 1234567890:</label>
                 <textarea 
                     id="description-edit" 
                     class="form-control"
